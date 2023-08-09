bb9dd09841ecbd1b1e88ba533f4fbc8776280752
Naruê Gonçalves Santos <naruegit@gmail.com>
2023-08-09 14:43:28 -0300

    feature: add e header home link

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function Header(){
   return(
     <header className="bg-detail fixed top-0 w-screen flex p-6 justify-between place-items-center rounded-b-3xl">
-      <h1 className="text-3xl">Next <code className="text-purple-700 bg-purple-100">Atômic</code> Site</h1>
+     <Link href={'/'}>
+      <h1 className="text-3xl">Next 
+      <code className="text-purple-700 bg-purple-100">Atômic</code> Site</h1>
+     </Link>
       <nav className="flex">
         <span className="px-3 cursor-pointer py-1 hover:scale-125 transition">exemplo</span>
         <span className="px-3 cursor-pointer py-1 hover:scale-125 transition">exemplo</span>
