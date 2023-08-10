d50e097365c7885547c5f1d81b7e47b5cc50281c
Naruê Gonçalves Santos <naruegit@gmail.com>
2023-08-09 21:32:00 -0300

    refactor: remove reverse background

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 export default function Card(props:CardProps) {
   return(
     <section className="flex justify-center mt-5">
-      <div className={`bg-gradient-to-${props.reverse? 'l' : 'r'} from-purple-800 to-detail gap-7 w-[80%] flex ${props.reverse? 'flex-row-reverse' : ''} p-6 shadow`}>
+      <div className={`bg-gradient-to-r from-purple-800 to-detail gap-7 w-[80%] flex ${props.reverse? 'flex-row-reverse' : ''} p-6 shadow`}>
         <div className={`w-[50%] h-full flex flex-col justify-center text-white`}>
           <h1 className="text-5xl font-bold">{props.title}</h1>
           <span>{props.content}</span>
